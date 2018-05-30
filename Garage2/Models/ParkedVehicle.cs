50d30cb35943f4063af3ff4882b3dffb119f5c66
Oscar Ljungdahl <oscar.ljungdahl@hotmail.com>
2018-05-30 16:12:19 +0200

    improved parkedVehicleclass

--- Garage2/Models/ParkedVehicle.cs
+++ Garage2/Models/ParkedVehicle.cs
@@ -8,14 +8,16 @@ namespace Garage2.Models
     public class ParkedVehicle
     {
         public enum VehicleTypeEnum { Car, Bus, Motorcycle }
-        public enum BrandEnum { Audi, Volov, Merzedes, Saab, BMW, Tesla }
+        public enum BrandEnum { Audi, Volvo, Merzedes, Saab, BMW, Tesla }
+        public enum ColorEnum { Green, White, Yellow, Black, Brown, Red, Blue }
+        public enum ModelEnum { Suv, SPort, CrossOver, Sedan }
 
         public int Id { get; set; }
         public VehicleTypeEnum VehicleType { get; set; }
         public BrandEnum Brand { get; set; }
-        public string Color { get; set; }
+        public ModelEnum Model { get; set; }
+        public ColorEnum Color { get; set; }
         public string RegNo { get; set; }
-        public string Model { get; set; }
         public int NoWheels { get; set; }
-    }   
+    }
 }
