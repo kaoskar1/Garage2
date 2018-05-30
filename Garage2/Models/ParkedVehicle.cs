3a86a6523aea0189b7a62e4887adb4ee429d3aba
Oscar Ljungdahl <oscar.ljungdahl@hotmail.com>
2018-05-30 15:08:16 +0200

    Add vehicle model.

---
+++ Garage2/Models/ParkedVehicle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Garage2.Models
+{
+    public class ParkedVehicle
+    {
+        public enum VehicleType { Car, Bus, Motorcycle }
+        public String Color { get; set; }
+        public int MyProperty { get; set; }
+    }   
+}
