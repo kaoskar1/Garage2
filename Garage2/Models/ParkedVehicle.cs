42939a66e6dfc28b46e832bd950a560f210c3574
Rolf B <rolf.bjarenstam@gmail.com>
2018-05-31 16:56:25 +0200

    Tidy up mess

--- Garage2/Models/ParkedVehicle.cs
+++ Garage2/Models/ParkedVehicle.cs
@@ -9,7 +9,6 @@ namespace Garage2.Models
     public class ParkedVehicle
     {
         public enum VehicleTypeEnum { Car = 1, Bus = 2, Motorcycle = 3 }
-        //public enum VehicleTypeEnum { Car, Bus, Motorcycle }
         public enum BrandEnum { Audi = 1, Volvo = 2 , Merzedes = 3, Saab = 4, BMW = 5, Tesla = 6 }
         public enum ColorEnum { Green = 1, White = 2, Yellow = 3, Black = 4, Brown = 5, Red = 6, Blue = 7}
         public enum ModelEnum { Suv = 1, Sport = 2,  CrossOver = 3, Sedan = 4 }
@@ -17,11 +16,11 @@ namespace Garage2.Models
         public int Id { get; set; }
 
         [Required]
-        [Range(1, 3, ErrorMessage = "Select ehat kind of vehicle ")]
+        [Range(1, 3, ErrorMessage = "Vehicle Type is required")]
         public VehicleTypeEnum VehicleType { get; set; }
 
         [Required]
-        [Range(1, 6, ErrorMessage = "Select What kind of brand you have")]
+        [Range(1, 6, ErrorMessage = "Brand is required")]
         public BrandEnum Brand { get; set; }
 
         [Range(1, 4, ErrorMessage = "Please select a car model")]
