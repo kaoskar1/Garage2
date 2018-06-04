bd98f4d19b4d7b848e2e377f3c126ac845ba14f6
Oscar Ljungdahl <oscar.ljungdahl@hotmail.com>
2018-06-04 14:20:17 +0200

    Add Receipt.

---
+++ Garage2/Models/Receipt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Garage2.Models
+{
+    public class Receipt
+    {
+        public String RegNo { get; set; }
+
+        public DateTime CheckInTime { get; set; }
+
+        public DateTime CheckOutTime { get; set; }
+    }
+}
