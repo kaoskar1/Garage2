7555166bbc238246494e88fdc844c912b0dfae7c
Rolf B <rolf.bjarenstam@gmail.com>
2018-06-04 14:24:09 +0200

    Add seed data

--- Garage2/Migrations/Configuration.cs
+++ Garage2/Migrations/Configuration.cs
@@ -1,8 +1,10 @@
 namespace Garage2.Migrations
 {
+    using Garage2.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
+    using System.Data.Entity.Validation;
     using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<Garage2.Models.Garage2Context>
@@ -13,7 +15,7 @@ namespace Garage2.Migrations
             ContextKey = "Garage2.Models.Garage2Context";
         }
 
-        protected override void Seed(Garage2.Models.Garage2Context context)
+        protected override void Seed(Models.Garage2Context dbContext)
         {
             //  This method will be called after migrating to the latest version.
 
@@ -27,6 +29,75 @@ namespace Garage2.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+            try
+            {
+                dbContext.ParkedVehicles.AddOrUpdate(
+                  p => p.RegNo,
+                  new ParkedVehicle
+                  {
+                      VehicleType = ParkedVehicle.VehicleTypeEnum.Car,
+                      Brand = ParkedVehicle.BrandEnum.Audi,
+                      Color = ParkedVehicle.ColorEnum.Black,
+                      Model = ParkedVehicle.ModelEnum.CrossOver,
+                      RegNo = "ABC123",
+                      NoWheels = 4,
+                      TimeStamp = DateTime.Now
+                  },
+                  new ParkedVehicle
+                  {
+                      VehicleType = ParkedVehicle.VehicleTypeEnum.Car,
+                      Brand = ParkedVehicle.BrandEnum.BMW,
+                      Color = ParkedVehicle.ColorEnum.Red,
+                      Model = ParkedVehicle.ModelEnum.Sedan,
+                      RegNo = "ABC124",
+                      NoWheels = 4,
+                      TimeStamp = DateTime.Now
+                  },
+                  new ParkedVehicle
+                  {
+                      VehicleType = ParkedVehicle.VehicleTypeEnum.Car,
+                      Brand = ParkedVehicle.BrandEnum.Merzedes,
+                      Color = ParkedVehicle.ColorEnum.White,
+                      Model = ParkedVehicle.ModelEnum.Sedan,
+                      RegNo = "ABC125",
+                      NoWheels = 4,
+                      TimeStamp = DateTime.Now
+                  },
+                  new ParkedVehicle
+                  {
+                      VehicleType = ParkedVehicle.VehicleTypeEnum.Car,
+                      Brand = ParkedVehicle.BrandEnum.Tesla,
+                      Color = ParkedVehicle.ColorEnum.Green,
+                      Model = ParkedVehicle.ModelEnum.Sedan,
+                      RegNo = "ABC126",
+                      NoWheels = 4,
+                      TimeStamp = DateTime.Now
+                  },
+                  new ParkedVehicle
+                  {
+                      VehicleType = ParkedVehicle.VehicleTypeEnum.Bus,
+                      Brand = ParkedVehicle.BrandEnum.Merzedes,
+                      Color = ParkedVehicle.ColorEnum.Yellow,
+                      RegNo = "ABC127",
+                      NoWheels = 6,
+                      TimeStamp = DateTime.Now
+                  }
+                );
+            }
+            catch (DbEntityValidationException e)
+            {
+                foreach (var eve in e.EntityValidationErrors)
+                {
+                    Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                        eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        Console.WriteLine("- Property: \"{0}\", Error: \"{1}\"",
+                            ve.PropertyName, ve.ErrorMessage);
+                    }
+                }
+                throw;
+            }
         }
     }
 }
