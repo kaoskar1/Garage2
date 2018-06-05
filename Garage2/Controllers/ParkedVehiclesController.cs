c5f87a8a7bc5f964c927f6fc3ec147471a9f6924
Oscar Ljungdahl <oscar.ljungdahl@hotmail.com>
2018-06-05 14:44:00 +0200

    fxied some text in recipt

--- Garage2/Controllers/ParkedVehiclesController.cs
+++ Garage2/Controllers/ParkedVehiclesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -15,9 +16,53 @@ namespace Garage2.Controllers
         private Garage2Context db = new Garage2Context();
 
         // GET: ParkedVehicles
-        public ActionResult Index()
+        public ActionResult ParkedVehicles(string sortOrder)
         {
-            return View(db.ParkedVehicles.ToList());
+            Debug.WriteLine("sortOrder: " + sortOrder);
+            ViewBag.RegNoSortParam = String.IsNullOrEmpty(sortOrder) ? "regno_desc" : "";
+            ViewBag.BrandSortParam = sortOrder == "brand" ? "brand_desc" : "brand";
+            var parkedVehicles = from s in db.ParkedVehicles
+                                 select s;
+            switch (sortOrder)
+            {
+                case "regno_desc":
+                    parkedVehicles = parkedVehicles.OrderByDescending(pv => pv.RegNo);
+                    break;
+                case "brand":
+                    parkedVehicles = parkedVehicles.OrderBy(pv => pv.Brand);
+                    break;
+                case "brand_desc":
+                    parkedVehicles = parkedVehicles.OrderByDescending(pv => pv.Brand);
+                    break;
+                default:
+                    parkedVehicles = parkedVehicles.OrderBy(pv => pv.RegNo);
+                    break;
+            }
+
+            return View(parkedVehicles.ToList());
+        }
+
+        // GET: ParkedVehicles/Receipt/1
+        public ActionResult Receipt(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            ParkedVehicle parkedVehicle = db.ParkedVehicles.Find(id);
+            if (parkedVehicle == null)
+            {
+                return HttpNotFound();
+            }
+
+            Receipt receipt = new Receipt
+            {
+                RegNo = parkedVehicle.RegNo,
+                CheckInTime = parkedVehicle.CheckInTime
+            };
+
+            return View(receipt);
         }
 
         // GET: ParkedVehicles/Details/5
@@ -35,24 +80,25 @@ namespace Garage2.Controllers
             return View(parkedVehicle);
         }
 
-        // GET: ParkedVehicles/Create
-        public ActionResult Create()
+        // GET: ParkedVehicles/CheckIn
+        public ActionResult CheckIn()
         {
             return View();
         }
 
-        // POST: ParkedVehicles/Create
+        // POST: ParkedVehicles/CheckIn
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,VehicleType,Brand,Color,RegNo,Model,NoWheels,CreatedAt")] ParkedVehicle parkedVehicle)
+        public ActionResult CheckIn([Bind(Include = "Id,VehicleType,Brand,Color,RegNo,Model,NoWheels")] ParkedVehicle parkedVehicle)
         {
             if (ModelState.IsValid)
             {
+                parkedVehicle.CheckInTime = DateTime.Now;
                 db.ParkedVehicles.Add(parkedVehicle);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("ParkedVehicles");
             }
 
             return View(parkedVehicle);
@@ -78,41 +124,52 @@ namespace Garage2.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,VehicleType,Brand,Color,RegNo,Model,NoWheels,CreatedAt")] ParkedVehicle parkedVehicle)
+        public ActionResult Edit([Bind(Include = "Id,VehicleType,Brand,Color,RegNo,Model,NoWheels,CheckInTime")] ParkedVehicle parkedVehicle)
         {
             if (ModelState.IsValid)
             {
+               parkedVehicle.CheckInTime = DateTime.Now;
                 db.Entry(parkedVehicle).State = EntityState.Modified;
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("ParkedVehicles");
             }
             return View(parkedVehicle);
         }
 
-        // GET: ParkedVehicles/Delete/5
-        public ActionResult Delete(int? id)
+        // GET: ParkedVehicles/CheckOut/5
+        public ActionResult CheckOut(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
+
             ParkedVehicle parkedVehicle = db.ParkedVehicles.Find(id);
             if (parkedVehicle == null)
             {
                 return HttpNotFound();
             }
+
+            //parkedVehicle.CheckOutTime = DateTime.Now;
+            //Receipt receipt = new Receipt
+            //{
+            //    CheckInTime = receipt.CheckInTime,
+            //    CheckOutTime = receipt.CheckOutTime,
+            //    RegNo = receipt.RegNo
+            //};
+
             return View(parkedVehicle);
         }
 
-        // POST: ParkedVehicles/Delete/5
-        [HttpPost, ActionName("Delete")]
+        // POST: ParkedVehicles/CheckOut/5
+        [HttpPost, ActionName("CheckOut")]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
+        public ActionResult CheckOutConfirmed(int id)
         {
             ParkedVehicle parkedVehicle = db.ParkedVehicles.Find(id);
             db.ParkedVehicles.Remove(parkedVehicle);
             db.SaveChanges();
-            return RedirectToAction("Index");
+            return RedirectToAction("ParkedVehicles");
         }
 
         protected override void Dispose(bool disposing)
